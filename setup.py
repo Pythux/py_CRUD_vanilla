73419a684c171396993d3343982ab458cf946064
Pythux <pythux@nowhere.org>
2022-04-03 18:17:36 +0200

    ModuleNotFoundError on submodule
    https://stackoverflow.com/questions/55381393/modulenotfounderror-on-submodule

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     description='database tools to link models to data_access',
     author='Pythux',
     # author_email='',
-    packages=['CRUD_vanilla'],  # same as name
+    packages=[
+        'CRUD_vanilla',
+        'CRUD_vanilla.connection',
+    ],  # same as name
     # install_requires=[],  # external packages as dependencies
 )
